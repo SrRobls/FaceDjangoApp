3ced9b507ff6224bb7cfb13f823d4ac03037e9f5
Johan Robles <johanrobles66@yahoo.com>
2023-02-10 12:49:28 -0500

    Diseño login y registro

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,17 +1,24 @@
 import React, { useState, useRef } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
 
 const Login = () => {
 
 
   const username = useRef()
   const password = useRef()
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState()
+  const [boolError, setBoolError] = useState(true)
+
 
   const LoginUser = async (userData) =>{
     try{
         let response = await axios.post('http://localhost:8000/api/autenticacion/login', userData)
         setMessage(response.data.message)
+        setBoolError(false)
         window.localStorage.setItem('info_user', await JSON.stringify(response.data))
         return response.data
     }catch(error){
@@ -34,21 +41,46 @@ const Login = () => {
   }
   }
 
+  function cerrar(){
+    setMessage()
+    setBoolError(true)
+  }
+
   return (
-    <div>
-      {message}
-        <form>
-          <label>
-            username: 
-            <input type="text" id='username' ref={username} required/>
-          </label>
-          <label>
-            password: 
-            <input type="password" id='password' ref={password} required/>
+    <div className='login d-flex flex-column align-items-center justify-content-center'>
+      <div className='title_port'>
+        <h1>FaceDango</h1>
+        <p>A social app project create with Django (DRF), ReactJs, Bootstrap and PostgreSQL. Enjoy it!.</p>
+      </div>
+      <Card className='form-login'>
+      <Card.Body className='d-flex flex-column align-items-center justify-content-center card-body'>
+        <Card.Title><h2>Login</h2></Card.Title>
+        <form  className='d-flex flex-column justify-content-center align-items-center' action="">
+          {message && boolError ? 
+            <Alert variant="danger" onClose={() => cerrar()} dismissible>
+               <Alert.Heading className='alert' ><p>{message}</p></Alert.Heading>
+            </Alert> : <div></div>}
+          {message && !boolError ? 
+            <Alert variant='success' onClose={() => cerrar()} dismissible>
+            <Alert.Heading className='alert' ><p>{message}</p></Alert.Heading>
+          </Alert> : <div></div>}
+          <label htmlFor="">
+            Username: &nbsp;
+            <input type="text"  ref={username}/>
+          </label><br />
+          <label htmlFor="">
+            Password: &nbsp;
+            <input type="password"  ref={password}/>
           </label>
         </form>
-        <button onClick={handleClick}>Entrar</button>
+        <div><br />
+          <Button onClick={handleClick}>Entrar</Button>&nbsp;&nbsp;
+          <Link to={'/registro'}><Button>Registrarse</Button></Link>
+        </div>
+      </Card.Body>
+      </Card>
     </div>
+    
   )
 }
 
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,6 +1,10 @@
 import React, {useState, useRef} from 'react'
 import { useHistory } from 'react-router-dom'
 import axios from 'axios'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
+
     const Register = () => {
         const history = useHistory()
         const [message, setMessage] = useState()
@@ -11,6 +15,7 @@ import axios from 'axios'
         const password = useRef()
         const confirmar_password = useRef()
         const url_imagen = useRef()
+        const [boolError, setBoolError] = useState(true)
 
         let irLogin = () =>{
             history.push('/')
@@ -21,6 +26,7 @@ import axios from 'axios'
                 let response = await axios.post('http://localhost:8000/api/autenticacion/registrar', userData)
                 setMessage('Creado!')
                 setTimeout(irLogin, 800)
+                setBoolError(false)
                 return response.data
             }catch(error){
                 console.log(error.response.status)
@@ -53,43 +59,60 @@ import axios from 'axios'
             }
         }
 
+        function cerrar(){
+          setMessage()
+          setBoolError(true)
+        }
+
       return (
-        <div>
-            {message}
-        <form className='form-registro'>
-          <label>
-            Username:&nbsp;
-            <input type="text" id='username' ref={username} required/>
-          </label>  
-          <label>
-            Nombre:&nbsp;  
-            <input type="text" id='nombre' ref={nombre} required/>
-          </label>
-          <label>
-            Apellido:&nbsp;  
-            <input type="text" id='apellido' ref={apellido} required/>
-          </label>
-          <label>
-            Email:&nbsp;
-            <input type="email" id='email' ref={email} required/>
-          </label>
-          <label>
-            password:&nbsp;
-            <input type="password" id='password' ref={password} required/>
-          </label>
-          <label>
-            confirmar password:&nbsp;
-            <input type="password" id='confirmar_password' ref={confirmar_password} required/>
-          </label>
-        </form>
-        <div>
-            <p>Pon la url de la foto de perfil que desees. Es opcional</p>
-            <label htmlFor="">
-                Url Imgagen: &nbsp;
-                <input type="text" name="" id="url_imagen" ref={url_imagen}/>
-            </label>
-        </div>
-        <button onClick={handleClick}>Registrar</button>
+        <div className=' registro d-flex justify-content-center align-items-center'>
+            <Card className='form-registro card mx-auto'>
+              <Card.Title><h2>Registro</h2></Card.Title>
+              <Card.Body className='card-body'>
+              <form className='d-flex flex-column justify-content-center align-items-center'>
+                <label>
+                  Username: <br/>
+                  <input type="text" id='username' ref={username} required/>
+                </label> 
+                <label>
+                  Nombre: <br/>  
+                  <input type="text" id='nombre' ref={nombre} required/>
+                </label>
+                <label>
+                  Apellido: <br/>  
+                  <input type="text" id='apellido' ref={apellido} required/>
+                </label>
+                <label>
+                  Email: <br/>
+                  <input type="email" id='email' ref={email} required/>
+                </label>
+                <label>
+                  password: <br/>
+                  <input type="password" id='password' ref={password} required/>
+                </label>
+                <label>
+                  confirmar password: <br/>
+                  <input type="password" id='confirmar_password' ref={confirmar_password} required/>
+                </label><br />
+            </form>
+            <div>
+                <p>Pon la url de la foto de perfil que desees. Es opcional</p>
+                <label htmlFor="">
+                    Url Imagen:&nbsp;
+                    <input type="text" name="" id="url_imagen" ref={url_imagen}/>
+                </label>
+            </div><br />
+            <Button onClick={handleClick}>Registrar</Button>
+              </Card.Body>
+              {message && boolError ? 
+                <Alert variant="danger" onClose={() => cerrar()} dismissible>
+                  <Alert.Heading className='alert' ><p>{message}</p></Alert.Heading>
+                </Alert> : <div></div>}
+              {message && !boolError ? 
+                <Alert variant='success' onClose={() => cerrar()} dismissible>
+                <Alert.Heading className='alert' ><p>{message}</p></Alert.Heading>
+              </Alert> : <div></div>}
+            </Card>
         </div>
       )
     }
